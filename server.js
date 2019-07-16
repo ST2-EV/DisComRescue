2ed3482ef2ad96dd9de841caf33af4139e0bb25a
Steve Paul <steve.s.paul22@gmail.com>
2019-07-16 11:15:06 +0530

    changed to cloudant as db

--- server.js
+++ server.js
@@ -1,57 +1,35 @@
+const fs = require("fs");
+const Cloudant = require("@cloudant/cloudant");
+const path = require("path");
 const express = require("express");
-const port = 3000;
-const connectDB = require("./config/db");
-const block = require('./models/blockchain');
 const app = express();
-
-connectDB();
-
-app.use(express.json({ extended: false }));
-
-app.post("/sendData", (req, res) => {
-    for(var i = 0; i < req.body.length; i++) {
-        var obj = json[i];
-        const {
-            index,
-            time,
-            locations, 
-            message,
-            nounce,
-            battery,
-            prehash
-          } = obj;
-      
-          const blockFields = {};
-          if (index) jobFields.CustomerName = CustomerName;
-          if (time) jobFields.MemoNumber = MemoNumber;
-          //@todo need to handle the json array passed in locations and added to the db
-          if (locations) jobFields.CustomerAddress = CustomerAddress;
-          if (message) jobFields.TelephoneNumber = TelephoneNumber;
-          if (nounce) jobFields.Equipment = Equipment;
-          if (battery) jobFields.Make = Make;
-          if (prehash) jobFields.Model = Model;
-          try {
-            //@todo code to update the chain if something new is getting added
-            // let job = await Job.findOne({ MemoNumber });
-            // if (job) {
-            //   job = await Job.findOneAndUpdate(
-            //     { MemoNumber: MemoNumber },
-            //     { $set: jobFields },
-            //     { new: true }
-            //   );
-            //   return res.json(job);
-            // }
-      
-            block = new Block(blockFields);
-            await block.save();
-            res.json(block);
-          } catch (err) {
-            console.error(err.message);
-            res.status(500).send("Server Error");
-          }
-    
-        
+const port = 3000;
+const cloudant = new Cloudant({
+  account: "1e04787e-e7b3-4fe1-9288-8b2d56aa29ad-bluemix",
+  plugins: {
+    iamauth: {
+      iamApiKey: "1OvWJHv1kqWuS6Suk-FSQtFDb1qnPZ_gpD73pPp9jhFK"
     }
-};
+  }
+});
+app.use(express.json({ extended: false }));
+app.post("/", (req, res) => {
+  const blockchain = req.body;
+  async function asyncCall() {
+    let surv = await cloudant.use("survivors");
+    return await surv.insert(blockchain);
+  }
+
+  asyncCall()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// let rawdata = fs.readFileSync(path.join(__dirname, "data", "blockchain.json"));
+// let blockchain = JSON.parse(rawdata);
